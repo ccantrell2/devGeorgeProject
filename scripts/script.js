c172b37038bc4daa5de34830bc52db5922d7f07f
Cohen Cantrell <167409296+ccantrell2@users.noreply.github.com>
2025-04-29 01:23:02 -0500

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,49 @@
+$(document).ready(function(){
+   
+  
+    $("#login").on("click", function(){
+     
+      let statusText = underConstruction("logout button");      
+      $("#login").text(statusText).prop(   "disabled", true);
+      
+    });
+
+    function underConstruction(status){
+      return "Logout"
+    };
+  }
+);
+
+fetch('../data/logData.json')
+.then(response => {
+    if (!response.ok) {
+        throw new Error('Network response was not ok');
+    }
+    return response.json(); 
+})
+.then(data => {
+    console.log(data);
+    displayLogData(data.logData);
+})
+.catch(error => {
+    console.error('Fetch Error :-S', error);
+}); 
+
+function displayLogData(logData) {
+    const container = $("#logDay");
+    container.empty();
+  
+    $.each(logData, (index, log) => {
+      container.append(`
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title"><strong>${log.date}</strong></h5>
+              <p class="card-text">${log.note}</p>
+              <a href="#" class="btn btn-primary">Edit</a>
+              <a href="#" class="btn btn-primary">Delete</a>
+            </div>
+          </div>
+      `);
+    });
+  }
+
